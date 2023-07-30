e6a569212a8355178ac044dfa709a3401e00278c
Adam Rashi <adamrashi@mail.ru>
2023-07-30 15:54:38 +0300

    Tests and README added

--- 5/main.py
+++ 5/main.py
@@ -2,12 +2,16 @@ import uvicorn
 from fastapi import FastAPI
 from json2xml import json2xml
 
+APP_PORT = 8001
+
 
 def get_application():
     app = FastAPI(title="Parser JSON to XML")
 
     @app.post("/transfer_json_to_xml")
     def transfer_json_to_xml(data: dict) -> str:
+        if not data:
+            return '<?xml version="1.0" ?><metadata></metadata>'
         return json2xml.Json2xml(data).to_xml()
 
     return app
@@ -15,4 +19,4 @@ def get_application():
 
 if __name__ == "__main__":
     app = get_application()
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=APP_PORT, log_level="info")
---
+++ 5/tests.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from fastapi.testclient import TestClient
+from main import get_application
+
+
+class TestJsonToXmlService(TestCase):
+    def setUp(self):
+        self.app = TestClient(get_application())
+
+    def test_valid_json_to_xml(self):
+        json_data = {"name": "John", "age": 30}
+        response = self.app.post("/transfer_json_to_xml", json=json_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('<name type=\\"str\\">John</name>', response.text)
+        self.assertIn('<age type=\\"int\\">30</age>', response.text)
+
+    def test_empty_json_to_xml(self):
+        empty_json_data = {}
+        response = self.app.post("/transfer_json_to_xml", json=empty_json_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text.strip('"'), '<?xml version=\\"1.0\\" ?><metadata></metadata>')
+
+    def test_invalid_json_data(self):
+        invalid_json_data = "invalid_json"
+        response = self.app.post("/transfer_json_to_xml", json=invalid_json_data)
+        self.assertEqual(response.status_code, 422)
