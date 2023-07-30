f41896a7b86218a0f73594dd7805fb9abc659819
dogekiller21 <dogekiller21@gmail.com>
2023-07-30 13:09:03 +0300

    test

---
+++ 1/backend/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {'result': 'fasd[pjkofasjkl[fasjklfaskjlasf'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=7000)
