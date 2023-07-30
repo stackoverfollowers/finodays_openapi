67ffe6b1f20690cae86e81cd1fa7ac52e9e632d8
Sergey Natalenko <sergey.natalenko@mail.ru>
2023-07-30 15:07:48 +0300

    Task 3. Done (need readme only)

--- 1/backend/main.py
+++ 1/backend/main.py
@@ -1,12 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+
 app = FastAPI()
 
 
 @app.get("/")
 def home():
-    return {'result': 'fasd[pjkofasjkl[fasjklfaskjlasf'}
+    return {"result": "fasd[pjkofasjkl[fasjklfaskjlasf"}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     uvicorn.run(app, port=7000)
